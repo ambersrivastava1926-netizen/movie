e31a0c36bd6f1519c61055d61031235812d7b237
ambersrivastava1926-netizen <ambersrivastava1926@gmail.com>
2026-01-17 22:34:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+import streamlit as st
+
+st.title("My First Streamlit App")
+
+st.write("Hello 👋 This is a simple Streamlit example.")
+
+name = st.text_input("Enter your name")
+
+if name:
+    st.success(f"Welcome, {name}!")
+
+value = st.slider("Select a number", 0, 100, 50)
+st.write("Selected value:", value)
 
