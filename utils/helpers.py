f3e96a8d8ce5153adca95bb0578cd19b8e093a18
ambersrivastava1926-netizen <ambersrivastava1926@gmail.com>
2026-02-08 23:23:47 +0530

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,10 +1,15 @@
 import pandas as pd
+from pathlib import Path
 from datetime import date
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 def load_players():
-    return pd.read_csv("data/players.csv")
+    return pd.read_csv(BASE_DIR / "data" / "players.csv")
 
 def calculate_age(dob):
     dob = pd.to_datetime(dob).date()
     today = date.today()
-    return today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+    return today.year - dob.year - (
+        (today.month, today.day) < (dob.month, dob.day)
+    )
